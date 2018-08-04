c2b1aa2e292e5e737bd3629103288af87cd6c5c4
El-Saig Dávid <daud.ici@gmail.com>
2018-08-04 22:19:15 +0200

    fix coding conventions in enter-staej.py and import_zip.py

--- enter-staej.py
+++ enter-staej.py
@@ -8,9 +8,10 @@ dir_tasks = os.path.join(dir_config, 'tasks')
 os.makedirs(dir_tasks, exist_ok=True)
 
 # look at arguments
-parser = argparse.ArgumentParser(description='STAEJ command line interface')
+parser = argparse.ArgumentParser(description='staej command line interface')
 parser.add_argument('zipfile', nargs='*', help='path of the JIGSAWS zip file')
-parser.add_argument('--db', action='store_const', const=True, default=False, help='create or recreate the working SQLite database')
+parser.add_argument('--db', action='store_const', const=True, default=False,
+                    help='create or recreate the working SQLite database')
 args = parser.parse_args()
 
 # copy database
@@ -18,21 +19,17 @@ file_db = os.path.join(dir_config, 'staej.sqlite')
 if args.db or not os.path.exists(file_db) :
     shutil.copyfile('template.sqlite', file_db)
 
-# just to get IPython's beautiful exception reports!
-#from IPython import embed
-#embed()
-
 if len(args.zipfile) > 0 :
-    from import_zip import extractVideos
+    from import_zip import extract_videos
     for file_name in args.zipfile :
         basename = os.path.basename(file_name).rpartition('.')[0]
         try :
-            print (basename, ':', 'start')
+            print(basename, ':', 'start')
 
-            name = extractVideos(file_name, globals())
+            name = extract_videos(file_name, globals())
 
-            print (basename, ':', 'done')
-        except StopIteration as e:#Exception as e :
-            print (basename,':', e);
+            print(basename, ':', 'done')
+        except StopIteration as e:  # Exception as e :
+            print(basename, ':', e)
             import traceback
             traceback.print_exc()
--- import_zip.py
+++ import_zip.py
@@ -1,59 +1,65 @@
 # depends: peewee,
 # optdepends: cython
 
-import os
 import zipfile
 import csv
 import model
 import model.database
+from os import path, makedirs
+from os.path import dirname
 
 EXCEPTION_INCORRECT_NAME = 'Incorrect archive, must have files like `TASK/video/VIDEO_capture.avi`'
 EXCEPTION_MISSING_META_FILE = 'Meta file is missing! Expected `meta_file_TASK.txt`.'
 
-DIR_CONFIG = 'dir_config'
 DIR_TASKS = 'dir_tasks'
 FILE_DB = 'file_db'
 
-def nextOrDefault(generator, default=None) :
-    'Gets the next item from the generator or the value of default if the generator stopped.'
-    try : return next(generator)
-    except StopIteration : return default
+
+def next_or_default(generator, default=None) :
+    """Gets the next item from the generator or the value of default if the generator stopped."""
+    try :
+        return next(generator)
+    except StopIteration :
+        return default
 
 
-def readBinaryAsText(binary_file, encoding = 'utf8'):
-    'Reads the contents of a binary file as text with the specified encoding.'
+def read_binary_as_text(binary_file, encoding='utf8'):
+    """Reads the contents of a binary file as text with the specified encoding."""
     while True:
-        line = binary_file.readline().decode('utf8')
-        if not line : return
+        line = binary_file.readline().decode(encoding)
+        if not line :
+            return
         yield line
 
-def readLinesAsCsv(file_descriptor, delimiter='\t', line_cb=None) :
-    ret = csv.reader(readBinaryAsText(file_descriptor), delimiter=delimiter)
+
+def read_lines_as_csv(file_descriptor, delimiter='\t', line_cb=None) :
+    ret = csv.reader(read_binary_as_text(file_descriptor), delimiter=delimiter)
     if line_cb is None :
         return ret
 
-    if line_cb == True :
-        line_cb = lambda coll : [ x for x in coll if x ]
-    elif line_cb == False :
-        line_cb = lambda coll : [ x for x in coll if not x ]
-    return ( line_cb(x) for x in ret )
+    if line_cb :
+        def line_cb(coll) : return [x for x in coll if x]
+    else :
+        def line_cb(coll) : return [x for x in coll if not x]
 
-def connectFileDb(config) :
+    return (line_cb(x) for x in ret)
+
+
+def connect_file_db(config) :
     file_db = config[FILE_DB]
     return model.database.connect(file_db)
 
-def extractVideos(filename, config) :
-    name = None
 
-    dir_config = config[DIR_CONFIG]
+def extract_videos(filename, config) :
     dir_tasks = config[DIR_TASKS]
 
     with zipfile.ZipFile(filename) as zf :
         # get task name and create video folder
-        name = os.path.dirname(nextOrDefault((x for x in zf.namelist() if x.endswith('/video/')), default='').rstrip('/'))
-        if not name: raise Exception()
-        dir_task = os.path.join(dir_tasks, name)
-        os.makedirs(dir_task, exist_ok=True)
+        name = dirname(next_or_default((x for x in zf.namelist() if x.endswith('/video/')), default='').rstrip('/'))
+        if not name:
+            raise Exception()
+        dir_task = path.join(dir_tasks, name)
+        makedirs(dir_task, exist_ok=True)
         print(0)
 
         # extract videos
@@ -61,7 +67,7 @@ def extractVideos(filename, config) :
         zf.extractall(path=dir_tasks, members=videos)
         print(1)
 
-        db = connectFileDb(config)
+        db = connect_file_db(config)
         gestures = dict(('G{}'.format(x.id), x) for x in model.database.Gesture.select())
         task = model.database.Task(name=name)
         task.save()
@@ -69,28 +75,29 @@ def extractVideos(filename, config) :
         print(2)
 
         # open meta file
-        meta = nextOrDefault( x for x in zf.namelist() if 'meta_file' in x.lower() )
-        if not meta : raise Exception(EXCEPTION_MISSING_META_FILE)
+        meta = next_or_default(x for x in zf.namelist() if 'meta_file' in x.lower())
+        if not meta :
+            raise Exception(EXCEPTION_MISSING_META_FILE)
         print(3)
         with zf.open(meta) as file_descriptor_meta :
             print(4)
 
-            #from IPython import embed
-            #embed()
+            # from IPython import embed
+            # embed()
 
-            for row in readLinesAsCsv(file_descriptor_meta) :
+            for row_meta in read_lines_as_csv(file_descriptor_meta) :
                 print(5)
-                video = model.database.Video.new(task.id, row)
-                #video.save(force_insert=True)
+                video = model.database.Video.new(task.id, row_meta)
+                # video.save(force_insert=True)
 
                 file_kinematics = next(x for x in zf.namelist() if '/kinematics/' in x and video.file_name in x)
                 with zf.open(file_kinematics) as file_descriptor_kinematics :
                     print(6)
                     frame = 0
-                    for row in readLinesAsCsv(file_descriptor_kinematics, delimiter=' ', line_cb=True) :
+                    for row_kinematics in read_lines_as_csv(file_descriptor_kinematics, delimiter=' ', line_cb=True) :
                         frame += 1
-                        kinematic = model.database.Kinematic.new(video.id, frame, row)
-                        if (kinematic.id % 10 == 0) :
+                        kinematic = model.database.Kinematic.new(video.id, frame, row_kinematics)
+                        if kinematic.id % 10 == 0 :
                             print(7, video.id , kinematic.id)
 
                 db.begin()
@@ -98,16 +105,11 @@ def extractVideos(filename, config) :
                 with zf.open(file_transcript) as file_descriptor_transcript :
                     print(8)
 
-                    for row in readLinesAsCsv(file_descriptor_transcript, delimiter=' ') :
-                        transcript = model.database.Transcript.new(task.id, video.id, row, gestures)
-                        if (transcript.id % 10 == 0) :
+                    for row_descriptor_transcript in read_lines_as_csv(file_descriptor_transcript, delimiter=' ') :
+                        transcript = model.database.Transcript.new(
+                            task.id, video.id, row_descriptor_transcript, gestures)
+                        if transcript.id % 10 == 0 :
                             print(9, video.id , transcript.id)
 
                 db.commit()
-
-
-
-
-
-
     return name
